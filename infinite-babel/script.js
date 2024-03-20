7df3444ff88bf33a3446636f6d61b1e55a40239a
Jacqueline Grimm <110108204+jacquelinegrimm@users.noreply.github.com>
2024-03-20 09:19:45 +0200

    Create script.js

---
+++ infinite-babel/script.js
@@ -0,0 +1,15 @@
+// script.js
+
+import { client } from "@gradio/client";
+
+const beginLink = document.getElementById("beginLink");
+const generatedTextDiv = document.getElementById("generatedText");
+
+beginLink.addEventListener("click", async () => {
+    const app = await client("jacquelinegrimm/mistralai-Mistral-7B-v0.1");
+    const result = await app.predict("/predict", [
+        "The universe (which others call the Library) is composed of an indefinite and perhaps infinite number of hexagonal galleries, with vast air shafts between, surrounded by very low railings.",
+    ]);
+
+    generatedTextDiv.innerHTML = result.data;
+});
