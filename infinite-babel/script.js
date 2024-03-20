0f26d19b38ea57e35a6551544ec8d6e08febc6df
Jacqueline Grimm <110108204+jacquelinegrimm@users.noreply.github.com>
2024-03-20 09:48:20 +0200

    Update script.js

--- infinite-babel/script.js
+++ infinite-babel/script.js
@@ -1,17 +1,18 @@
 // script.js
 
-document.addEventListener("DOMContentLoaded", async () => {
+(async () => {
     const generatedTextDiv = document.getElementById("generatedText");
 
     try {
         const app = await gradio.client("jacquelinegrimm/mistralai-Mistral-7B-v0.1");
         const result = await app.predict("/predict", [
-            "The universe (which others call the Library) is composed of an indefinite and perhaps infinite number of hexagonal galleries, with vast air shafts between, surrounded by very low railings.",
+            "Hello!!",
         ]);
 
         generatedTextDiv.innerHTML = result.data;
+        console.log(result.data);
     } catch (error) {
         console.error("Error:", error);
         generatedTextDiv.innerHTML = "An error occurred while generating the text.";
     }
-});
+})();
