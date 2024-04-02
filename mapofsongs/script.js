729fd945aa59dd79fb91cad905272daa0ff63173
Jacqueline Grimm <110108204+jacquelinegrimm@users.noreply.github.com>
2024-04-03 00:58:52 +0300

    Update script.js

--- mapofsongs/script.js
+++ mapofsongs/script.js
@@ -1,48 +1,95 @@
 fetch("data.json")
-.then(response => response.json())
-.then(data => {
-  // Create the plot
-  const plotData = data.map(point => ({
-    type: "scattergeo",
-    mode: "markers",
-    lat: [point.latitude],
-    lon: [point.longitude],
-    hovertext: point.hovertext,
-    marker: {
-      size: 15,
-      color: point.color
-    },
-    name: ""
-  }));
+  .then(response => response.json())
+  .then(data => {
+    // Create the plot
+    const plotData = data.map(point => ({
+      type: "scattergeo",
+      mode: "markers",
+      lat: [point.latitude],
+      lon: [point.longitude],
+      hovertext: point.hovertext,
+      marker: {
+        size: 15,
+        color: point.color
+      },
+      name: ""
+    }));
 
-  const layout = {
-    geo: {
-      projection: {
-        type: "orthographic",
-        scale: 0.65,
-        rotation: {
-          lon: -80,
-          lat: 10
+    const frames = [];
+    for (let i = -80; i <= 280; i += 10) {
+      frames.push({
+        layout: {
+          geo: {
+            projection: {
+              rotation: {
+                lon: i,
+                lat: 10
+              }
+            }
+          }
         }
-      }
-    },
-    width: 800,
-    height: 800,
-    margin: {
-      r: 0,
-      t: 0,
-      l: 0,
-      b: 0
-    },
-    showlegend: false
-  };
+      });
+    }
 
-  const config = {
-    displayModeBar: false
-  };
+    const layout = {
+      geo: {
+        projection: {
+          type: "orthographic",
+          scale: 0.65,
+          rotation: {
+            lon: -80,
+            lat: 10
+          }
+        }
+      },
+      width: 800,
+      height: 800,
+      margin: {
+        r: 0,
+        t: 0,
+        l: 0,
+        b: 0
+      },
+      showlegend: false
+    };
 
-  Plotly.newPlot("map", plotData, layout, config);
-})
-.catch(error => {
-  console.error("Error loading JSON data:", error);
-});
+    const config = {
+      displayModeBar: false
+    };
+
+    Plotly.newPlot("map", plotData, layout, config).then(function() {
+      Plotly.animate("map", frames, {
+        transition: {
+          duration: 0
+        },
+        frame: {
+          duration: 50,
+          redraw: false
+        }
+      }, {
+        mode: "immediate",
+        transition: {
+          duration: 0
+        },
+        frame: {
+          duration: 50,
+          redraw: false
+        }
+      }).then(function() {
+        // Reset globe after the animation
+        Plotly.update("map", {}, {
+          geo: {
+            projection: {
+              rotation: {
+                lon: -80,
+                lat: 10
+              }
+            }
+          }
+        });
+      });
+    });
+  })
+  .catch(error => {
+    console.error("Error loading JSON data:", error);
+  });
