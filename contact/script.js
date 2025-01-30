7e61d022344a907b90106cf114d678f013cef05e
Jacqueline <110108204+jxqln@users.noreply.github.com>
2025-01-29 18:04:05 -0600

    Update script.js

--- contact/script.js
+++ contact/script.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const contactLink = document.getElementById('contact-link');
     contactLink.addEventListener('click', function(e) {
         e.preventDefault();
-        const username = 'jacquelinekgrimm';
-        const domain = 'gmail.com';
+        const username = 'contact';
+        const domain = 'jacqueline.dev';
         window.location.href = `mailto:${username}@${domain}`;
     });
 });
