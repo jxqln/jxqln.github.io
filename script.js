bf6be991710257c55f35ff26d88d4a4aa78956cd
Jacqueline <110108204+jxqln@users.noreply.github.com>
2025-02-03 18:08:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(gener
 gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy);
 gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy);
 gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy);
-gui.domElement.style.marginTop = '62px';
+gui.domElement.style.marginTop = '60px';
 
 gui.close();
 
