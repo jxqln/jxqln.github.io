00aa089b9915197049288393d041c7925338ffda
Jacqueline <110108204+jxqln@users.noreply.github.com>
2025-02-03 18:15:29 -0600

    Delete contact directory

--- contact/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const contactLink = document.getElementById('contact-link');
-    contactLink.addEventListener('click', function(e) {
-        e.preventDefault();
-        const username = 'contact';
-        const domain = 'jacqueline.dev';
-        window.location.href = `mailto:${username}@${domain}`;
-    });
-});
